d98b3be93442d9761369a6b03b5367748c11368e
mariacpa <123938854+mariacpa@users.noreply.github.com>
2023-08-21 18:54:09 +0300

    testing bootstrap

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-        <WeatherSearch />
-      </header>
-      <footer className="App-footer">
-        <a
-          href="https://github.com/mariacpa/weather-app-v3"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-sourced Code
-        </a>
-        <span> by Maria Padilla</span>
-      </footer>
+      <div classname="container">
+        <header className="App-header">
+          <h1>Weather App</h1>
+          <WeatherSearch />
+          <button classname="btn btn-primary">Hello</button>
+        </header>
+        <footer className="App-footer">
+          <a
+            href="https://github.com/mariacpa/weather-app-v3"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced Code
+          </a>
+
+          <span> by Maria Padilla</span>
+        </footer>
+      </div>
     </div>
   );
 }
