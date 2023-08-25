c15a332d94240f8f3bd312fa91de0618e73e71a5
mariacpa <123938854+mariacpa@users.noreply.github.com>
2023-08-25 10:44:38 +0300

    api integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,57 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather border">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type city name"
-              className="form-control"
-              autoFocus="on"
-            ></input>
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            ></input>
-          </div>
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState({ready: false});
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: Math.round(response.data.main.temp),
+      humidity: Math.round(response.data.main.humidity),
+      wind: Math.round(response.data.wind.speed),
+      description: response.data.weather[0].description,
+      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      date: "21 Aug, 18:56",
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "5293d8454b519c30f6f6331f38c85b4c";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(handleResponse);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <div className="row">
+        <div className="col-9">
+          <input
+            type="search"
+            placeholder="Type city name"
+            className="form-control"
+            autoFocus="on"
+            onChange={updateCity}
+          ></input>
         </div>
-      </form>
-      <div className="weather-info">
-        <h1>Cali</h1>
-        <ul>
-          <li>21 Aug, 18:56</li>
-          <li>Mostly sunny</li>
-        </ul>
-        <div className="row">
-          <div className="col-6">
-            <div className="clearfix">
-              <img
-                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                alt="cloudy"
-                className="float-start"
-              ></img>
-              <div className="float-start">
-                <span className="temperature">6</span>
-                <span className="unit">°C</span>
+        <div className="col-3">
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-primary-edited w-100"
+          ></input>
+        </div>
+      </div>
+    </form>
+  );
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather border">
+        {form}
+
+        <div className="weather-info">
+          <h1 className="text-capitalize">{city}</h1>
+          <ul>
+            <li>{weatherData.date}</li>
+            <li className="text-capitalize">{weatherData.description}</li>
+          </ul>
+          <div className="row">
+            <div className="col-6">
+              <div className="clearfix">
+                <img
+                  src={weatherData.iconUrl}
+                  alt={weatherData.description}
+                  className="float-start"
+                />
+                <div className="float-start">
+                  <span className="temperature">{weatherData.temperature}</span>
+                  <span className="unit">°C</span>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Precipitation:0%</li>
-              <li>Humidity: 60%</li>
-              <li>Wind: 14 km/h</li>
-            </ul>
+            <div className="col-6">
+              <ul>
+                <li>Humidity: {weatherData.humidity}%</li>
+                <li>Wind: {weatherData.wind} km/h</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return form;
+  }
 }
